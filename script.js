39df2f92e69041724a06facc3b565d512eafeef1
Zen <135819176+Zen12102004@users.noreply.github.com>
2026-01-28 10:21:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("#navigation li").forEach(li => {
+        li.style.cursor = "pointer";
+
+        li.addEventListener("click", () => {
+            const text = li.textContent.trim();
+            const url = "Sites/" + text + ".html";
+            window.location.href = url;
+        });
+    });
+});
