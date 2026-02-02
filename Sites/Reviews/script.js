31317f2a24db34d9e0b14578d4ef38afe5cc18ad
Zen <135819176+Zen12102004@users.noreply.github.com>
2026-02-02 11:41:46 +0530

    Add files via upload

--- Sites/Reviews/script.js
+++ Sites/Reviews/script.js
@@ -9,3 +9,15 @@ function showChapter(chapterNumber) {
         content.style.display = "none";
     }
 }
+
+function showMovie(movieNumber){
+    const content = document.getElementById(`movie${movieNumber}-content`);
+
+    if (!content) return;
+
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
